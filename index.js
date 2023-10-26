b96a58bbe4cce2009554f88876a3e9e8fe16870a
ramseypapii <ramsey.kuwona@student.moringaschool.com>
2023-10-26 11:38:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Make a fetch request to the Game of Thrones API
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => renderBooks(data)); // Call renderBooks with the JSON data
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const bookList = document.getElementById("list"); // Assuming you have an HTML element with the id "list"
+
+  // Iterate through the books and create list items
+  books.forEach((book) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = book.name;
+    bookList.appendChild(listItem);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  function fetchBooks() {
-    fetch('https://anapioficeandfire.com/api/books')
-      .then((response) => response.json())
-      .then((data) => renderBooks(data));
-  }
-  document.addEventListener('DOMContentLoaded', function() {
-    fetchBooks();
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks();
 });
